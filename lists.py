65d967a9c026c2fd7d21f8c635a6297c5f93e749
Anusha2321 <anushabandiatmakur2005@gmail.com>
2025-03-21 08:57:22 +0530

    lists operations.py

--- lists.py
+++ lists.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 13 09:05:36 2024
-
-@author: shanm
-"""
-
+l1=[1,2,[3,4],5]
+l2=[6,7,8]
+print("nested list:",l1)
+print("length:",len(l1))
+print("concatenate:",l1+l2)
+print("membership:", 3 in l1)
+print("indexing:",l1[0])
