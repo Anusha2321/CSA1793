bfca43ca7d2e45bb0d3e8b75e048bc3794f48cb5
Anusha2321 <anushabandiatmakur2005@gmail.com>
2025-03-21 10:52:27 +0530

    Update set operations.py

--- set operations.py
+++ set operations.py
@@ -4,3 +4,9 @@ print("union:",a|b)
 print("intersection:",a&b)
 print("difference(a-b):",a-b)
 print("symmetric:",a^b)
+
+#output
+union: {1, 2, 3, 4, 5, 6}
+intersection: {3, 4}
+difference(a-b): {1, 2}
+symmetric: {1, 2, 5, 6}
