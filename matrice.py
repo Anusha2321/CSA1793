a800905fca30dbbb2b54b9232045057bf35d4c6b
Anusha2321 <anushabandiatmakur2005@gmail.com>
2025-03-21 10:48:26 +0530

    Update matrice.py

--- matrice.py
+++ matrice.py
@@ -7,3 +7,14 @@ b = np.array([[5, 6], [7, 8]])
 print('Sum:\n', a + b)
 print('Transpose of A:\n', a.T)
 print('Transpose of B:\n', b.T)
+
+#outptut
+Sum:
+ [[ 6  8]
+ [10 12]]
+Transpose of A:
+ [[1 3]
+ [2 4]]
+Transpose of B:
+ [[5 7]
+ [6 8]]
