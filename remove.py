5eb005d500c67e98d663b1ce123819f452fca6cb
Anusha2321 <anushabandiatmakur2005@gmail.com>
2025-03-21 10:51:55 +0530

    Update remove.py

--- remove.py
+++ remove.py
@@ -1,2 +1,7 @@
 text = input("Enter text: ")
 print("Without punctuation:", "".join(c for c in text if c.isalnum() or c.isspace()))
+
+
+#output
+Enter text:  hii how are how's it going
+Without punctuation:  hii how are hows it going
