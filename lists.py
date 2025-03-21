b742d5adc5c827c92d6cdcdcca0f46dfaf618d04
Anusha2321 <anushabandiatmakur2005@gmail.com>
2025-03-21 10:47:01 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -5,3 +5,11 @@ print("length:",len(l1))
 print("concatenate:",l1+l2)
 print("membership:", 3 in l1)
 print("indexing:",l1[0])
+
+
+#output
+nested list: [1, 2, [3, 4], 5]
+length: 4
+concatenate: [1, 2, [3, 4], 5, 6, 7, 8]
+membership: False
+indexing: 1
